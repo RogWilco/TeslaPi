d5b1465a8999add20e8dfc1908dbedf40bfe1c21
Nick Williams <git@nickawilliams.com>
2019-04-08 14:04:05 -0700

    Fleshed out installer script.

--- setup/install.sh
+++ setup/install.sh
@@ -1,26 +1,143 @@
 #!/usr/bin/env bash
 
-# Set Hostname: TeslaPi
+#?/synopsis     install [subcategory]
+#?/summary      executes post-install tasks for a fresh Raspbian system
 
-# Enable SSH
-sudo touch /boot/ssh
+#?/description
+#? Executes all post-install steps by default, unless one or more categories
+#? are specified. Available categories are:
+#?
+#?   packages       Installs all non-default packages, adding additional
+#?                  PPA sources as necessary.
+#?   system         Performs all system customization tasks.
+#?   user           Performs all user customization tasks.
+#?   docker         Installs Docker and Docker Compose.
+#?   reload         Reloads all services and resources affected by this
+#?                  post-install script.
 
-# Enable Headless Wireless Networking
-# copy wpa_supplicant.conf to /boot/wpa_supplicant.conf
+sudo -v
 
-# Install haveget (entropy generator that doesn't require a mouse)
-# for VNC Server to start when headless.
-sudo apt install -y haveged
+# ============================================================================
+# Setup
+# ============================================================================
+config_hostname="TeslaPi"
+config_timezone="America/Los_Angeles"
 
-sudo apt update
-sudo apt install -y realvnc-vnc-server
+user=$(id -un)
+home=$(realpath ~)
 
-# Enable VNC Server via `sudo raspi-config` => Interfacing Options > VNC > Yes
+linkup() {
+	if [ $# -lt 2 ]
+	then
+		return 1
+	fi
 
-# Downgrade VNC Server's authentication scheme
-# /root/.vnc/config.d/vncserver-x11
-# 	- Replace Authentication=SystemAuth with
-#		Authentication=VncAuth
-#	- run `sudo vncpasswd -service`
-#		- set password
-#	- Restart VNC Server
+	local target="$1"
+	local symlink="$2"
+	local directory=''
+
+	directory=$(dirname "$symlink")
+
+	# If linking a directory, delete an existing directory at that location.
+	rm -rf "$symlink"
+
+	# Backfill nonexistent directories in symlink's path.
+	mkdir -p "$directory"
+
+	# Create new symlink.
+	ln -Fs "$target" "$symlink"
+}
+
+apt::repository::add() {
+	for i in "$@"; do
+		grep -h "^deb.*$i" /etc/apt/sources.list.d/* > /dev/null 2>&1
+
+		if [ "$?" -ne 0 ]; then
+			echo "Adding Repository: $i"
+			sudo apt-add-repository -y "$i"
+		else
+			echo "Repository Exists: $i"
+		fi
+	done
+}
+
+# ============================================================================
+# Packages
+# ============================================================================
+install::packages() {
+    # Apt Update
+	sudo apt update -y
+	sudo apt upgrade -y
+
+    # Install Packages
+    local packages=(
+        haveged                     # Entropy Generator (for headless VNC)
+        jq                          # Command-line JSON Processor
+        zsh                         # Zshell
+    )
+
+    sudo apt install -y "${packages[@]}"
+}
+
+# ============================================================================
+# System Customization
+# ============================================================================
+install::system() {
+    sudo raspi-config nonint do_change_timezone "${config_timezone}"
+    sudo raspi-config nonint do_hostname "${config_hostname}"      # Set hostname
+    sudo raspi-config nonint do_ssh 0                       # Enable SSH
+    sudo raspi-config nonint do_vnc 0                       # Enable VNC Server
+
+    # Downgrade VNC Server's auth scheme, set password, and restart service.
+    sudo sed -i '/^Authentication/s/=.*$/=VncAuth/' /root/.vnc/config.d/vncserver-x11
+    sudo vncpasswd -service
+    sudo systemctl restart vncserver-x11-serviced.service
+
+    sudo chsh -s "$(which zsh)"                    # Default system shell
+	sudo chsh -s "$(which zsh)" "$user"            # Default user shell
+}
+
+# ============================================================================
+# User Customization
+# ============================================================================
+install::user() {
+    echo "Not Implemented"
+}
+
+# ============================================================================
+# Docker
+# ============================================================================
+install::docker() {
+    sudo bash <(curl -fsSL https://get.docker.com -o get-docker.sh)
+
+	sudo apt update -y
+	sudo apt install -y docker-compose
+
+	sudo usermod -aG docker "$USER"
+}
+
+# ============================================================================
+# Reload Affected Services
+# ============================================================================
+install::reload() {
+	source ~/.bash_profile
+}
+
+# ============================================================================
+# Execute Tasks
+# ============================================================================
+if [[ $# -eq 0 ]]; then
+	install::packages
+	install::system
+	install::docker
+	install::user
+	install::reload
+else
+	for arg in "$@"; do
+		type=$(type -t "install::${arg}")
+
+		if [ -n "${type}" ] && [ "${type}" = "function" ]; then
+			eval "install::${arg}"
+		fi
+	done
+fi
